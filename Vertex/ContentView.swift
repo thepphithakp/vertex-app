2eb18b86047932f8b688be1d6bc8bf40095adff8
Thepphithak-p <thepphithak.ph@gmail.com>
2026-08-07 21:58:50 +0700

    Initial Commit

---
+++ Vertex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vertex
+//
+//  Created by thepphithak phadjuntheuk on 7/8/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vertex/VertexApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertexApp.swift
+//  Vertex
+//
+//  Created by thepphithak phadjuntheuk on 7/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct VertexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
